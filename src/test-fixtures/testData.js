b244a4e051ad8f02b167e50f77d613cb8126817d
James Chartrand <jc.chartrand@gmail.com>
2023-09-11 15:23:26 -0400

    allow multi-cred upload/setup

---
+++ src/test-fixtures/testData.js
@@ -0,0 +1,15 @@
+import testVC from './testVC.js';
+
+const getDataForExchangeSetupPost = (tenantName) => {
+  const fakeData = {
+    tenantName,
+    exchangeHost: 'http://localhost:4005',
+    data: [
+      { vc: testVC, retrievalId: 'someId',  },
+      { vc: testVC, retrievalId: 'blah' }
+    ]
+  }
+  return fakeData
+}
+
+export { getDataForExchangeSetupPost }
